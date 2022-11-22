3808cc5834bb6ffe2df32ecc472168ef0d22dcc3
rxbnDE <git@rxbn.de>
2022-11-22 19:25:45 +0100

    add cronjob to update heatmap

--- app/server.js
+++ app/server.js
@@ -4,3 +4,27 @@ dotenv.config();
 
 // run start api
 require('./bin/www')
+
+const db = require('./db/module');
+db.connect();
+
+const CronJob = require('cron').CronJob;
+job = new CronJob(
+	'15 */2 * * * *',
+	function() {
+		(async () => {
+			currentState = await db.getCurrentState()
+			day = new Date().getDay();
+			hour = new Date().getHours();
+			minute = Math.floor((new Date().getMinutes())/5)*5;
+
+			await db.initializeHeatmap();
+			reply = await db.updateHeatMap(day, hour, minute, (await db.getCurrentState()).reply[0].value);
+
+			console.log("updated heatmap");
+		})()
+	},
+	null,
+	true,
+	'Europe/Berlin'
+);
