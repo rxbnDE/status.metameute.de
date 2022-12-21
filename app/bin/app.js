7d112120f86f2c43f0bd8a706da679c56d6eb922
rxbnDE <git@rxbn.de>
2022-12-21 11:16:43 +0100

    app: fix dir paths

--- app/bin/app.js
+++ app/bin/app.js
@@ -7,14 +7,14 @@ var logger = require('morgan');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 var exp = async () => {
 	router = require('../routes/index');
