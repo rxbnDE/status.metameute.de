5597996b561bc8e2d550b223c3f05497b5d60eb6
rxbnDE <git@rxbn.de>
2023-01-20 10:46:59 +0100

    quick fix: duration since last state change

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,7 +11,7 @@ function timeSince(date) {
 	days = Math.floor(seconds / 86400);
 	seconds -= days * 86400;
 	hours = Math.floor(seconds / 3600);
-	seconds -= hours * 86400;
+	seconds -= hours * 3600;
 	minutes = Math.floor(seconds / 60);
 	seconds -= minutes * 60;
 
